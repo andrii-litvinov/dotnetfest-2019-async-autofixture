6ed815bf2e71ae106e24eff1e8b1b79b6fb4a02a
Andrii Litvinov <andrii.litvinov@gmail.com>
2019-11-26 18:21:47 +0200

    Fix issue when query parameters are not being deserialized into the query object

--- Demo/Service/Handlers/Requests/QueryRequestHandler.cs
+++ Demo/Service/Handlers/Requests/QueryRequestHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Service.Handlers.Requests
 {
@@ -7,6 +9,12 @@ namespace Service.Handlers.Requests
     public abstract class QueryRequestHandler<T> : ControllerBase where T : class
     {
         [HttpGet]
-        public abstract Task<IActionResult> Handle([FromRoute, FromQuery] T query);
+        public abstract Task<IActionResult> Handle([FromUri] T query);
+    }
+
+    public class FromUriAttribute : Attribute, IBindingSourceMetadata
+    {
+        public BindingSource BindingSource { get; } = CompositeBindingSource.Create(
+            new[] {BindingSource.Path, BindingSource.Query}, nameof(FromUriAttribute));
     }
 }
